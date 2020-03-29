053d7c629b9bb04388d1df228fbe23e6fdb85ad4
Taylor <wwwaaavvveeerrr@gmail.com>
2020-03-28 22:16:48 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.sass';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        under construction
+      </header>
+    </div>
+  );
+}
+
+export default App;
