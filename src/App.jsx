3b77959a7b4834e1a12dd31407b8036060346b58
ElijahMcKay <elijahmckay10@gmail.com>
2020-04-03 23:16:01 -0500

    fix: debugging why I cannot receive response from POST request

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 // import submitSent from "./utility/submitSend";
 import axios from "axios";
+import Loader from "react-loader-spinner"
 
 // styling
 import "./App.scss";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 // image imports
 import Phone from "./assets/phone.svg";
@@ -19,6 +21,13 @@ const App = () => {
     phone: ""
   });
 
+  // using a hook to handle Loading state
+  const [status, setStatus] = useState({
+    isLoading: false,
+    success: false,
+    failure: false
+  });
+
   const handleChange = event => {
     setState({ 
       ...state, 
@@ -28,11 +37,37 @@ const App = () => {
   const handleSubmit = event => {
     event.preventDefault();
 
+    // switching status to tStatus so the loader animation shows up
+    setStatus({
+      ...status,
+      isLoading: true
+    });
+
+    let axiosConfig = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
     axios
-      .post(`${process.env.REACT_APP_API_URL}/sms/web`, state)
-      .then(res => { console.log(res) })
-      .catch(err => { console.log(err) });
+      .post(`${process.env.REACT_APP_API_URL}/sms/web`, state, { 'Content-Type': 'application/json' })
+      .then(res => { 
+        console.log('success')
+        setStatus({
+          ...status,
+          success: true,
+          isLoading: false
+        });
+      })
+      .catch(err => { 
+        console.log(err)
+        setStatus({
+          ...status,
+          success: false,
+          isLoading: false
+        });
+      });
   };
+
   console.log(state)
   return (
     <div className="App">
@@ -49,24 +84,59 @@ const App = () => {
               <br />
               No sign-up required.
             </p>
+            <label htmlFor="phone"></label>
             <input
               type="tel"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               className="form-input"
               placeholder="ex. 555-555-5555"
               name="phone"
+              id="phone"
               onChange={handleChange}
               value={state.phone}
+              required
             />
+            <label htmlFor="zip">Zip Code</label>
             <input
               type="number"
+              min="10"
               className="form-input"
               placeholder="ex. 90210"
               name="zip"
+              id="zip"
               onChange={handleChange}
               value={state.zip}
+              required
             />
             <div className="btn-wrapper">
-              <button className="submit-btn">Update Me</button>
+              {status.isLoading ? (<button className="submit-btn">
+                  <Loader
+                    type="Oval"
+                    color="#FFFFFF"
+                    height={25}
+                    width={25}
+                    timeout={30000} //10 secs
+                    style={{ marginTop: ".2rem" }}
+                  />
+                </button>
+              ) : (
+                <button
+                  className="submit-btn"
+                >
+                  Update Me
+                </button>
+              )}
+              {status.success ? (
+                <h2 className="messages messages-success">
+                  Success!  You should be getting a message from us any second.
+                </h2>
+              ) : null}
+              {status.failure ? (
+                <h2 className="messages">
+                  Something went wrong.  Make sure you are using a 5 digit zip code.
+                </h2>
+              ) : null}
+              {/* <button className="submit-btn">Update Me</button> */}
             </div>
           </form>
         </div>
