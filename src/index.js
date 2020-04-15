1a26703b251a2c1dc5cf95f0515fe170c8183603
kyle Guerrero <kyleguerrero85@gmail.com>
2020-04-15 13:18:03 -0500

    chore: got captcha put in place
    got the recaptcha rendering to the DOM, moving onto logic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// GOOGLE CAPTCHA
+import { GoogleReCaptchaProvider as CaptchaProvider } from 'react-google-recaptcha-v3'
 
-ReactDOM.render(<App />,
+ReactDOM.render(
+  <CaptchaProvider
+    reCaptchaKey={process.env.REACT_APP_CAPTCHA_KEY}
+  >
+    <App />
+  </CaptchaProvider>,
   document.getElementById('root')
 );
