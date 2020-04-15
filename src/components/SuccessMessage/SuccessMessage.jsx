fd50a6d47ab9da83c6adc22883b9caa34a769181
Harsh Desai <hurshd0@gmail.com>
2020-04-15 15:19:21 -0500

    text: fixed typos for all the text message (#46)

--- src/components/SuccessMessage/SuccessMessage.jsx
+++ src/components/SuccessMessage/SuccessMessage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function SuccessMessage(){
-  return(
+export default function SuccessMessage() {
+  return (
     <div className="success-container">
       <h2 className="status">Success!</h2>
       <p className="message">You should be receiving a message from us any second.</p>
-      <p className="instructions">Just reply to our text with another zip code to get updates for that area.</p>
+      <p className="instructions">Just reply to our text with another ZIP code to get updates for that area.</p>
     </div>
   )
 }
