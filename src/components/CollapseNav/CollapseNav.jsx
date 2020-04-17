02554c953335e7ac1c595b41b95364fe1c2f0952
kyle Guerrero <kyleguerrero85@gmail.com>
2020-04-16 20:53:49 -0500

    chore: started on dropdown animation

--- src/components/CollapseNav/CollapseNav.jsx
+++ src/components/CollapseNav/CollapseNav.jsx
@@ -4,8 +4,8 @@ function CollapseNav({ toggle }) {
 
   return (
 
-    <div className="collapse-nav-container" style={{ "display": toggle ? "flex" : "none" }}>
-      <nav className="collapse-nav">
+    <div className={`collapse-nav-container ${toggle ? "fadeInDown": ""}`} style={{ "display": toggle ? "flex" : "none" }}>
+      <nav className="collapse-nav" style={{ "display": toggle ? "none" : "hidden" }}>
         <a className="collapse-nav-link" href="https://covid19-us-staging.herokuapp.com/" alt="mobile-home">
           Home
           </a>
